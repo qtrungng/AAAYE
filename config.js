ce3b477cfa504fbd2c7bd81d4e23a5216f1a4d63
qtrungng <86765061+qtrungng@users.noreply.github.com>
2021-07-01 13:58:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
     introTitle: 'Túy Oanhh ahh! Vợ yêu của anh ',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    introDesc: `Trái đất vốn lạ thường.
+    Mà sao em cứ đi nhầm đường.
+    Lạc vào tim anh lẻ loi.
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh :)) ',
     btnNo: ' Lovee loveee uu :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh ',
